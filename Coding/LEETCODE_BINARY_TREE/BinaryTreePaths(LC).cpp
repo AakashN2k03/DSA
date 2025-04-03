98eb2b675178f38cfda43d3d4986bb4b775e1b68
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-04-03 15:52:33 +0530

    Create BinaryTreePaths(LC).cpp->striver

---
+++ Coding/LEETCODE_BINARY_TREE/BinaryTreePaths(LC).cpp
@@ -0,0 +1,38 @@
+// LEETCODE: 257. Binary Tree Paths (RECURSION , INORDER )
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+     void root_to_leaf_path(TreeNode* node,string temp,vector<string>& ans)
+     {
+        if(!node) return;
+        if(!temp.empty()) temp+="->";
+        temp+=to_string(node->val);
+
+
+        if(!node->left && ! node->right) ans.push_back(temp);
+
+        root_to_leaf_path(node->left,temp,ans);
+        root_to_leaf_path(node->right,temp,ans);
+
+        temp.pop_back();
+
+     }
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> ans;
+        root_to_leaf_path(root,"",ans);
+        return ans;
+        
+    }
+};

c8657fd6ed06c5316273fcc8b533c3bc41435724
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-04-06 15:10:11 +0530

    Create ValidateBinarySearchTree(LC).cpp->striver

---
+++ Coding/LEETCODE_BINARY_SEARCH_TREE/ValidateBinarySearchTree(LC).cpp
@@ -0,0 +1,27 @@
+// LEETCODE: 98. Validate Binary Search Tree 
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+   bool checkisValidBST(TreeNode* node,long min_val,long max_val)
+   {
+       if(!node) return true;
+       if(node->val <= min_val || node->val >= max_val) return false;
+
+       return checkisValidBST(node->left,min_val,node->val) && checkisValidBST(node->right,node->val,max_val);
+   }
+    bool isValidBST(TreeNode* root) {
+        return checkisValidBST(root,LONG_MIN,LONG_MAX);
+    }
+};

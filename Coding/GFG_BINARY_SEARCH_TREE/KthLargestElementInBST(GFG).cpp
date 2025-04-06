14b0fc762cc68ff347a4a96b9dd2f5ae6c7afd40
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-04-06 14:36:10 +0530

    Create KthLargestElementInBST(GFG).cpp->self+Striver

---
+++ Coding/GFG_BINARY_SEARCH_TREE/KthLargestElementInBST(GFG).cpp
@@ -0,0 +1,26 @@
+// GFG: Kth largest element in BST (INORDER IN REVERSE)
+
+class Solution {
+  public:
+   void reverseInorder(Node* root,int k,int& count,int& largest)
+   {
+       if(!root || count>=k) return;
+       reverseInorder(root->right,k,count,largest);
+       
+       count++;
+       
+       if(count==k)
+       {
+           largest=root->data;
+       }
+        reverseInorder(root->left,k,count,largest);
+       
+   }
+    int kthLargest(Node *root, int k) {
+        int count,largest;
+        count=0;
+        largest=-1;
+        reverseInorder(root,k,count,largest);
+        return largest;
+    }
+};

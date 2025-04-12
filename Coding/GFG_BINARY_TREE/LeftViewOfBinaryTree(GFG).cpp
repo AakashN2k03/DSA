19371733d26157adf4063ca79f0fba515a7d17aa
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-04-12 16:00:35 +0530

    Create LeftViewOfBinaryTree(GFG).cpp->self

---
+++ Coding/GFG_BINARY_TREE/LeftViewOfBinaryTree(GFG).cpp
@@ -0,0 +1,21 @@
+GFG: Left View of Binary Tree ( DFS, RECURSION)
+
+// SAME AS RIGHT VIEW .JUST SINGLE CHANGE.
+
+class Solution {
+  public:
+   void left(Node* root,int level,vector<int>& ans)
+   {
+    if(!root) return;
+    if(ans.size()==level) ans.push_back(root->data);
+    left(root->left,level+1,ans);  // HERE IS THE CHANGE
+    left(root->right,level+1,ans);
+
+   }
+    vector<int> leftView(Node *root) {
+      vector<int> ans;
+        int level=0;
+        left(root,level,ans);
+        return ans;
+    }
+};

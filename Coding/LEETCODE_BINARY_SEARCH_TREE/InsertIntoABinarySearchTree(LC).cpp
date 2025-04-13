19ec82572cc88af5c74ce097e4972ebfebe07d1f
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-04-13 17:47:17 +0530

    Update InsertIntoABinarySearchTree(LC).cpp

--- Coding/LEETCODE_BINARY_SEARCH_TREE/InsertIntoABinarySearchTree(LC).cpp
+++ Coding/LEETCODE_BINARY_SEARCH_TREE/InsertIntoABinarySearchTree(LC).cpp
@@ -1,17 +1,5 @@
 // LEETCODE: 701. Insert into a Binary Search Tree 
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
@@ -41,3 +29,7 @@ public:
         return root;
     }
 };
+
+// Time Complexity: O(h)
+//  Space Complexity: O(1)
+     

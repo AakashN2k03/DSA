d175f366c2104cdecd8a1be904e06494821b86d6
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-04-20 15:18:45 +0530

    Create CourseSchedule(GFG).cpp->striver+self

---
+++ Coding/GFG_GRAPH/CourseSchedule(GFG).cpp
@@ -0,0 +1,46 @@
+// GFG: Course Schedule (KANNS'S ALGORITHM / BFS )
+
+class Solution {
+  public:
+    vector<int> findOrder(int N, vector<vector<int>> prerequisites) {
+         vector<int> indegree(N);
+        vector<vector<int>> adjls(N);
+        
+        for (auto it : prerequisites) {
+           int u=it[0];
+           int v=it[1];
+           adjls[v].push_back(u);  // eg {0, 1}, since task 0 depends on task 1, hence the edge should be 1 -> 0
+        }
+        
+        for (int i = 0; i < N; i++) {
+            for (auto adjnode : adjls[i]) {
+                indegree[adjnode]++;
+            }
+        }
+        
+        queue<int> qu;
+        for (int i = 0; i < N; i++) {
+            if (indegree[i] == 0) qu.push(i);
+        }
+        vector<int>ans;
+        
+        int counter = 0;
+        while (!qu.empty()) {
+            int node = qu.front();
+            qu.pop();
+            counter++;
+           ans.push_back(node);
+            
+            for (auto adjnode : adjls[node]) {
+                indegree[adjnode]--;
+                if (indegree[adjnode] == 0) qu.push(adjnode);
+            }
+        }
+        
+        if(counter==N) return ans;
+        return {};
+        
+    }
+};
+// ✅ Time Complexity: O(V + E)
+// ✅ Space Complexity: O(N)

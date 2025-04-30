4724cb134eb1a50ca44d455f9babb0539adca628
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-04-30 22:26:31 +0530

    Create UniquePaths(LC).cpp->STRIVER

---
+++ Coding/LEETCODE_DYNAMIC_PROGRAMMING/UniquePaths(LC).cpp
@@ -0,0 +1,50 @@
+// LEETCODE: 62. Unique Paths ( DP,MEMOIZATION,TABULATION)
+//NOTE: DO THIS PROBLEM USING SPACE OPTIMIZATION APPROACH
+class Solution {
+public:
+    int allpossiblepaths(int i,int j,vector<vector<int>>&dp)
+    {
+        if(i==0 || j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int up=allpossiblepaths(i-1,j,dp);
+        int down=allpossiblepaths(i,j-1,dp);
+         
+         return dp[i][j]=up+down;
+      
+    }
+    int uniquePaths(int m, int n) {
+       vector<vector<int>> dp(m,vector<int>(n,-1));
+        return allpossiblepaths(m-1,n-1,dp);
+        
+    }
+};
+//  Time Complexity: O(m * n)
+//  Space Complexity: = O(m * n) (for dp) + O(m + n) (for recursion stack)
+// But we usually consider the dominating term: O(m * n).
+
+//TABULATION
+class Solution {
+public:
+    
+    int uniquePaths(int m, int n) {
+       vector<vector<int>> dp(m,vector<int>(n,-1));
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 || j==0) dp[i][j]=1;
+                else{
+                    int up=dp[i-1][j];
+                    int down=dp[i][j-1];
+                    dp[i][j]=up+down;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+        
+    }
+};
+// Time Complexity: O(m * n)
+// Space Complexity: O(m * n) // NO STACK SPACE

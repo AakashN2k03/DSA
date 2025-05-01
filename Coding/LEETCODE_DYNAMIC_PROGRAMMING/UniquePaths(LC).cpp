2df189dcfe73957751decc5282107d924477977b
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-05-01 13:40:47 +0530

    Update UniquePaths(LC).cpp

--- Coding/LEETCODE_DYNAMIC_PROGRAMMING/UniquePaths(LC).cpp
+++ Coding/LEETCODE_DYNAMIC_PROGRAMMING/UniquePaths(LC).cpp
@@ -4,7 +4,7 @@ class Solution {
 public:
     int allpossiblepaths(int i,int j,vector<vector<int>>&dp)
     {
-        if(i==0 || j==0) return 1;
+        if(i==0 || j==0) return 1; // can use if(i==0 && j==0) return 1;
         if(i<0 || j<0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
 

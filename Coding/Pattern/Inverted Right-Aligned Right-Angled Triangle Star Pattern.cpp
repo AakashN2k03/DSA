180de7b47047a6137708102c91a8fbb2c3ef983a
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-06-01 20:57:45 +0530

    Create Inverted Right-Aligned Right-Angled Triangle Star Pattern.cpp

---
+++ Coding/Pattern/Inverted Right-Aligned Right-Angled Triangle Star Pattern.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int n;
+   cout<<"enter the number"<<endl;
+   cin>>n;
+  
+   for(int row=n;row>0;row--)
+   {
+       for(int space=1;space<=(n-row);space++)
+       {
+           cout<<" ";
+       }
+       for(int col=1;col<=row;col++)
+       {
+           cout<<"*";
+       }
+       cout<<"\n";
+   }
+
+    return 0;
+}

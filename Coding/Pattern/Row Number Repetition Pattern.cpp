65be898159c6cdbe43f3b6686c56dd20215a78f6
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-06-01 19:25:00 +0530

    Create Row Number Repetition Pattern.cpp

---
+++ Coding/Pattern/Row Number Repetition Pattern.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int n;
+   cout<<"enter the number"<<endl;
+   cin>>n;
+   for(int row=1;row<=n;row++)
+   {
+       for(int col=1;col<=n;col++)
+       {
+           cout<<row<<" ";
+       }
+       cout<<"\n";
+   }
+
+    return 0;
+}

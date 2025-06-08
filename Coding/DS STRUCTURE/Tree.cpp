f2a1e5d1994d74b86e4cd4afd0a7580434704441
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-06-08 17:30:32 +0530

    Update Tree.cpp

--- Coding/DS STRUCTURE/Tree.cpp
+++ Coding/DS STRUCTURE/Tree.cpp
@@ -42,3 +42,47 @@ int main()
 //       2   4
 //      /     \
 //     3       5
+
+
+
+// USING CLASS
+
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+	int val;
+	Node* left;
+	Node* right;
+	
+	Node(int data)
+	{
+		val=data;
+		left=nullptr;
+		right=nullptr;
+	}
+};
+
+Node* buildtree()
+{
+	Node* root=new Node(1);
+	root->left=new Node(2);
+	root->left->left=new Node(3);
+	root->right=new Node(4);
+	root->right->right=new Node(5);
+	return root;
+}
+void inorder(Node* root)
+{
+	if(!root) return;
+	inorder(root->left);
+	cout<<root->val;
+	inorder(root->right);
+}
+int main()
+{
+	Node*root=buildtree();
+	cout<<" inorder traversal"<<endl;
+	inorder(root);
+}

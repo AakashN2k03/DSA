a8d2a97895d3445eefe7e59a7db96cbedc668d6e
AakashN2k03 <156887140+AakashN2k03@users.noreply.github.com>
2025-06-08 17:28:48 +0530

    Create Tree.cpp

---
+++ Coding/DS STRUCTURE/Tree.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+	int val;
+	Node* left;
+	Node* right;
+	
+	Node(int data)
+	{
+		val=data;
+		left=nullptr;
+		right=nullptr;
+	}
+};
+
+Node* buildtree()
+{
+	Node* root=new Node(1);
+	root->left=new Node(2);
+	root->left->left=new Node(3);
+	root->right=new Node(4);
+	root->right->right=new Node(5);
+	return root;
+}
+void inorder(Node* root)
+{
+	if(!root) return;
+	inorder(root->left);
+	cout<<root->val;
+	inorder(root->right);
+}
+int main()
+{
+	Node*root=buildtree();
+	cout<<" inorder traversal"<<endl;
+	inorder(root);
+}
+
+// eg:     1
+//        / \
+//       2   4
+//      /     \
+//     3       5
